69cee695ade032db618f14a65ffb8faf76f7407b
seoungwonseoungwon <destiny_023@naver.com>
2023-04-18 17:04:01 +0900

    대문 페이지 수정하기

--- single_pages/views.py
+++ single_pages/views.py
@@ -3,10 +3,17 @@ from blog.models import Post
 
 # Create your views here.
 def landing(request):
-    posts = Post.objects.all().order_by('-pk')
-
-    return render(request, 'single_pages/landing.html', {'posts':posts})
-
+    recent_posts = Post.objects.order_by('-pk')[:3]
+    return render(
+        request,
+        'single_pages/landing.html',
+        {
+        'recent_posts':recent_posts
+        }
+    )
 
 def about_me(request):
-    return render(request, 'single_pages/about_me.html')
+    return render(
+        request,
+        'single_pages/about_me.html'
+    )
