f54d610786acbeb6a48d4edd171e0ac632b27b58
seoungwonseoungwon <destiny_023@naver.com>
2023-04-18 13:46:05 +0900

    태그 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    path('tag/<str:slug>/', views.tag_page),
     path('category/<str:slug>/', views.category_page),
     path('',views.PostList.as_view(), name="blog"),
     path('<int:pk>/',views.PostDetail.as_view(), name="post_detail"),
