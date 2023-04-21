bf5022af16d3a9425c7743c23bbfaea61eab3416
seoungwonseoungwon <destiny_023@naver.com>
2023-04-21 18:04:49 +0900

    dashboard 페이지 수정

--- static/js/main.js
+++ static/js/main.js
@@ -1,55 +1,17 @@
-const ctx = document.getElementById('yongsan');
-  
-  const labels = [
-    {% for cd in yongsan_datas %}
-    '{{ cd.restaurant }}',
-    {% endfor %}
-  ]
-  
-  const pop_data = [
-    {% for cd in yongsan_datas %}
-    {{ cd.name_count }},
-    {% endfor %}
-  
-  ]
-  
-  const data = {
-    labels: labels,
-    datasets: [{
-      label: '냥냥',
-      data: pop_data,
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-        'rgba(255, 205, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(201, 203, 207, 0.2)'
-      ],
-      borderColor: [
-        'rgb(255, 99, 132)',
-        'rgb(255, 159, 64)',
-        'rgb(255, 205, 86)',
-        'rgb(75, 192, 192)',
-        'rgb(54, 162, 235)',
-        'rgb(153, 102, 255)',
-        'rgb(201, 203, 207)'
-      ],
-      borderWidth: 1
-    }]
-  };
-  
-  const config = {
-    type: 'bar',
-    data: data,
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    },
-  };
-  
-  new Chart(ctx, config)
+// nav높이 + container(contents) 높이 + footer 높이 < viewport 높이
+//    :  footer 요소를 viewport 아래에 fixed
+// nav높이 + container높이 + footer높이 >= viewport높이
+//    :  footer 요소의 fixed 속성을 제거함
+let nav_h = document.querySelector('.navbar').clientHeight;
+let con_h = document.querySelector('div.contents').clientHeight;
+let footer_h = document.querySelector('footer').clientHeight;
+// navbar, container, footer 높이 콘솔 출력
+console.log(nav_h, con_h, footer_h)
+// viewport 높이 콘솔 출력
+console.log(window.innerHeight) 
+// html 콘텐츠 문서의 높이
+doc_h = nav_h + con_h + footer_h
+// 높이의 따른 fixed-bottom 처리
+if (doc_h >= window.innerHeight){
+  document.querySelector('footer').classList.remove('fixed-bottom')
+}
